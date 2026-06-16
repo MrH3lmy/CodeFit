055f4ec32de26381861189f72256c2261fa6dbbf
Ahmed Helmy <63620526+MrH3lmy@users.noreply.github.com>
2026-06-17 01:44:26 +0300

    Add technical card templates

--- src/main/java/com/codefit/model/CardType.java
+++ src/main/java/com/codefit/model/CardType.java
@@ -1,13 +1,23 @@
 package com.codefit.model;
 
 public enum CardType {
-    RECALL("Recall"),
-    COMMAND("Command");
+    RECALL("Concept flashcard"),
+    COMMAND("Command"),
+    LINUX_COMMAND("Linux command"),
+    GIT_COMMAND("Git command"),
+    SQL_QUERY("SQL query"),
+    REGEX_PATTERN("Regex pattern"),
+    CODE_OUTPUT("Code output prediction"),
+    CONCEPT("Concept flashcard");
 
     private final String label;
 
     CardType(String label) {
         this.label = label;
+    }
+
+    public boolean isCommandTemplate() {
+        return this == COMMAND || this == LINUX_COMMAND || this == GIT_COMMAND;
     }
 
     @Override
